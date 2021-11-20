138d61ffe234cb2f93870fd5a243f192c8ba540e
liho9253 <timmy89566@gmail.com>
2021-11-21 01:18:53 +0800

    20211121

--- Practice_1/src/WokingWuthJacaDataTypes_2/WW_1.java
+++ Practice_1/src/WokingWuthJacaDataTypes_2/WW_1.java
@@ -29,6 +29,19 @@ public class WW_1 {
         }         
         return 1;//coupon*offset*base*value/100; 變數沒有初始值
     }
+        //4
+        //abstract int t ;  
+        static int sa ; 
+        final static private double PI = 3.14159265358979323846 ;
+        final Object[ ] objArr = { null } ; 
+        //abstract void format( ) ;
+        //5
+        public static Integer wiggler(Integer x){
+            Integer y = x + 10;
+            x++;
+            System.out.println(x);
+            return y;
+        }
     public static void main(String[] args) {
         /*//1
         switch(getSwitch(args[0])){
@@ -43,6 +56,11 @@ public class WW_1 {
         obj = new Object();
         obj = null;
         tc.doSomething(obj);
+        //5
+        Integer dataWrapper = new Integer(5);
+        Integer value = wiggler(dataWrapper);
+        System.out.println(dataWrapper+value);
+        
     }
     
 }
--- Practice_1/src/WokingWuthJacaDataTypes_2/WW_2.java
+++ Practice_1/src/WokingWuthJacaDataTypes_2/WW_2.java
@@ -9,34 +9,54 @@ package WokingWuthJacaDataTypes_2;
  *
  * @author user
  */
-public class WW_2 {
-    class Fruit{     
-        int seeds;    
-        String taste;     
-        boolean edible; 
-    }  
-    class TropicalFruit extends Fruit{
-        double weight;
-        TropicalFruit(){
-            seeds = 0;
-            taste = "sweet";
-            weight = 10.0;
-        }
-    }  
-    class Banana extends TropicalFruit{
-        double calories;
-        Banana(){
-            seeds = 10;
-            weight = 20.0;
-            calories = 200;
-        } 
-    }  
-    public class TestClass {
-        public void main(String[] args) {
-            Banana b = new Banana();         
-            System.out.println(b.edible+" "+b.weight+" "+b.calories);
-        }
+
+class Fruit{     
+    int seeds;    
+    String taste;     
+    boolean edible; 
+}  
+class TropicalFruit extends Fruit{
+    double weight;
+    TropicalFruit(){
+        seeds = 0;
+        taste = "sweet";
+        weight = 10.0;
+    }
+}  
+class Banana extends TropicalFruit{
+    double calories;
+    Banana(){
+        seeds = 10;
+        weight = 20.0;
+        calories = 200;
     } 
-   
-    
-}
+}  
+
+public class WW_2 {    
+    //3
+    public static void testInts(Integer obj, int var){
+        obj = var++;
+        obj++;
+    }         
+    public static void main(String[] args) {
+        // 1
+        Banana b = new Banana();         
+        System.out.println(b.edible+" "+b.weight+" "+b.calories);
+        System.out.println("====================================");
+        // 2
+        int i, j, k;
+        i = j = k = 9;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println("====================================");
+        //3
+        Integer val1 = new Integer(5);
+        int val2 = 9;         
+        testInts(val1++, ++val2);
+        System.out.println(val1+" "+val2);
+    }
+} 
+
+
+
