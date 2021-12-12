4efc5169af1587841b89d8ebc9d74f4146faa9db
liho9253 <timmy89566@gmail.com>
2021-12-13 00:08:07 +0800

    20211212

--- Practice_1/src/WorkingWithInheritance/WWI_3.java
+++ Practice_1/src/WorkingWithInheritance/WWI_3.java
@@ -5,17 +5,44 @@
  */
 package WorkingWithInheritance;
 
-/**
- *
- * @author user
- */
+interface Flyer{ 
+    String getName(); 
+}  
+class Bird implements Flyer{
+    public String name;
+    public Bird(String name){
+        this.name = name;     
+    }     
+    public String getName(){
+        return name; 
+    } 
+}  
+class Eagle extends Bird {
+    public Eagle(String name){
+        super(name);     
+    } 
+}
+
+class A12{
+    public static void sM1() {
+         System.out.println("In base static");
+    } 
+} 
+class B32 extends A12{
+     public static void sM1() {  System.out.println("In sub static"); }  
+    // public  void sM1() {  System.out.println("In sub non-static"); } 
+    // static 不能被 non-static 覆蓋    non-static 也不能被 static 覆蓋 
+}
 public class WWI_3 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        Flyer f = new Eagle("American Bald Eagle");
+        System.out.println(f.getName()); 
+        System.out.println(((Eagle)f).name);
+        System.out.println(((Bird)f).getName());
     }
     
 }
