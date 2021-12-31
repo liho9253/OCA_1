034db5464761b2b1205acc19d99e7a7795003c11
liho9253 <timmy89566@gmail.com>
2021-12-31 23:39:37 +0800

    20211231

---
+++ Practice_1/src/InstanceofOperator/ioo_2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InstanceofOperator;
+
+/**
+ *
+ * @author user
+ */
+public class ioo_2 {
+
+    //  java.util.Date 和 java.lang.String.沒有直接關係
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
