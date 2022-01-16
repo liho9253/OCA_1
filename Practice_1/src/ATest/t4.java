09384c351a1a44a48642ed7a826237b2989997be
liho9253 <timmy89566@gmail.com>
2022-01-16 23:22:49 +0800

    20220116

---
+++ Practice_1/src/ATest/t4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ATest;
+
+/**
+ *
+ * @author user
+ */
+public class t4 {
+    // 抽象方法 後面直接;
+    // static 為共用物件
+    // 非受檢例外 RuntimeException, error 無規定
+    // 受檢例外 RuntimeException, error 以外的全部 都要try catch 或 throws
+    // static 不能直接用變數
+    // fimal 不能被繼承
+    // 建構子封裝跟方法封裝
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
