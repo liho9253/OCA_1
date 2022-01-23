8aff62df9933d5e200d9fecb9b6e781c242e1457
liho9253 <timmy89566@gmail.com>
2022-01-23 22:36:03 +0800

    20220123

--- Practice_1/src/ATest/t5.java
+++ Practice_1/src/ATest/t5.java
@@ -10,10 +10,19 @@ package ATest;
  * @author user
  */
 public class t5 {
-
-    /**
-     * @param args the command line arguments
-     */
+    // 合法轉型 c1 obj1 = (c1)父 c2子;
+    // switch 沒有continue
+    // 物件的基本型態為null 
+    // static方法 只能呼叫 static變數
+    // 兩物件相等就要覆寫 equals 和 hashcode
+    // indexOf 抓不到就回傳-1
+    // default 同資料夾才可以用  protected 同資料夾 子類別
+    // obj instanceif p1 ==> (p1)obj
+    // equals 比較 Object
+    // equals 要先覆寫 不然作用跟 == 一樣
+    // String 的 equals 裡面已經自己寫好條件 
+    // this() super()只能放在建構子
+    // 定義在for迴圈裡 就不能在for迴圈外面使用
     public static void main(String[] args) {
         // TODO code application logic here
     }
