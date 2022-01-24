cd21a6fc00f3159165facd84299d89aa2afeda9d
liho9253 <timmy89566@gmail.com>
2022-01-24 22:42:01 +0800

    20220124

---
+++ Practice_1/src/LambdaExpressions/le_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LambdaExpressions;
+
+/**
+ *
+ * @author user
+ */
+public class le_2 {
+
+    // Lambda 寫法  
+    //   printUsefulData(al, d-> d.value>2 ); 
+    //   printUsefulData(al, (Data d)-> { return d.value>2; }  );
+    //   () -> System.out.println("running...")
+    public static void main(String[] args) {
+        
+    }
+    
+}
