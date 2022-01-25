9b082fe381467c2a3dd573ac0f0552abefae5935
liho9253 <timmy89566@gmail.com>
2022-01-25 23:50:44 +0800

    20220125

---
+++ Practice_1/src/CH1/ch_1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CH1;
+import java.util.AbstractCollection;
+// import 一定要在 package 後面
+// 同一個 package 可以不用 import
+// 有 static 會先執行
+// 只能是 import static
+/**
+ *
+ * @author user
+ */
+public class ch_1 {
+
+    //  int a; 
+    //  static int f( ) { return a; } 因為 a 不是 static 所以不能回傳 a
+    // public class 的名稱要跟檔案名稱一樣
+    public static void main(String[] args) {
+        // public ch_1 oc = new ch_1(); 前面不能加public 
+    }
+}
