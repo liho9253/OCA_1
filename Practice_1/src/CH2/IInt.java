5bd966a13091a5197281d42ee3c83d717ef9cae0
liho9253 <timmy89566@gmail.com>
2022-01-27 01:51:34 +0800

    20220127

---
+++ Practice_1/src/CH2/IInt.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CH2;
+
+/**
+ *
+ * @author user
+ */
+public class ch_2  implements IInt{
+    // abstract 不能有 final static private
+    // 轉型時 (父)子
+    // 一個 new 一個 object
+    // interface class enums 都是類型
+    // java 可以類型"types"多重繼承  不能狀態"State"的多重繼承
+    public static void main(String[] args) {
+        int l = thevalue;  
+    } 
+}
+public interface IInt{
+    int thevalue = 0; 
+}
