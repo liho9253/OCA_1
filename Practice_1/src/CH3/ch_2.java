ad79782eab9e21853c0a425ef649e24b48c6e64e
liho9253 <timmy89566@gmail.com>
2022-02-02 23:13:51 +0800

    20220202

--- Practice_1/src/CH3/ch_2.java
+++ Practice_1/src/CH3/ch_2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CH1;
+package CH3;
 
 /**
  *
@@ -14,11 +14,8 @@ public class ch_2 {
     /**
      * @param args the command line arguments
      */
-     
-    final static public void main (String[ ] arguments ) {
-        // main mthod 可以是final 也可以return 一定要有[]
-        long a1 = 10l;
-        System.out.println(a1);
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
     
 }
--- Practice_1/src/CreatingAndUsingArrays/CUA_2.java
+++ Practice_1/src/CreatingAndUsingArrays/CUA_2.java
@@ -18,7 +18,7 @@ public class CUA_2 {
         //String[] sA1 = new String[1] { "aaa"};
         String[] sA2 = new String[] { "aaa"};
         String[] sA3 = new String[1] ; 
-        sA3[0] = "aaa";
+        sA3[0] = "aa"; 
         String[] sA4 = {new String( "aaa")};
         String[] sA5 = { "aaa"};
         //---------------------------------------
