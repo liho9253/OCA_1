8f737aa1027bb4e0792845899676d5512912cc0e
liho9253 <timmy89566@gmail.com>
2022-02-05 03:47:04 +0800

    20220205

--- Practice_1/src/CH3/ch_2.java
+++ Practice_1/src/CH3/ch_2.java
@@ -5,17 +5,33 @@
  */
 package CH3;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author user
  */
 public class ch_2 {
- 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static int[] x = new int[0];
+    static{
+        // x[0] = 10; java.lang.ExceptionInInitializerError
+    }
+    public static void incr(int   n ) { n++ ; }
+    public static void incr(int[ ] n ) { n [ 0 ]++ ; }
+    public static void main(String[] args) { 
+        
+        int ia[][][] = new int[4][3][3];
+        System.out.println(ia.length+" "+ia[0].length+" "+ia[1][0].length);
+        
+        int i = 1;
+        int[] iArr = {1};
+        incr(i) ;
+        incr(iArr) ;
+        System.out.println( "i = " + i + "  iArr[0] = " + iArr [ 0 ] ) ;
+        
+        Object[] name1 = new Object[]{ "aaa", new Object(), new ArrayList(), 10};
+        Object[] name2 = new Object[]{ "aaa", new Object(), new ArrayList(),  new String[]{""} };
+        //Object[] name3 = new Object[]{ "aaa", new Object(), new ArrayList(), {}};
     }
     
 }
