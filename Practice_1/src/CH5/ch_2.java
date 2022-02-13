5b7e9ef0043a78ea95ab349a6a0bb9e3ae0549e3
liho9253 <timmy89566@gmail.com>
2022-02-13 18:40:48 +0800

    20220213

---
+++ Practice_1/src/CH5/ch_2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CH5;
+
+/**
+ *
+ * @author user
+ */
+public class ch_2 {
+
+    // RuntimeExceptions 不用 catch
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
