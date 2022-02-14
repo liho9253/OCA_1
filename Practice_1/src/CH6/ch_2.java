1453bee5b424073b9ab9477c99728646086bf57d
liho9253 <timmy89566@gmail.com>
2022-02-14 22:56:30 +0800

    20220214

---
+++ Practice_1/src/CH6/ch_2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CH6;
+
+/**
+ *
+ * @author user
+ */
+public class ch_2 {
+
+    // charAt 最大值是 length-1
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.ensureCapacity(100);
+    }
+    
+}
