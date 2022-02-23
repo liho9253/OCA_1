832f83ea55785890f821d6d3f47c1372de6e595d
liho9253 <timmy89566@gmail.com>
2022-02-23 23:39:46 +0800

    20220223

--- Practice_1/src/Tough/ch_1.java
+++ Practice_1/src/Tough/ch_1.java
@@ -13,16 +13,19 @@ class AA extends A {
  // public long getCode(){ return 3;}
 }
 public class ch_1 {
+    
     static ch_1 ref;  
     String[] arguments;
     public void func(String[] args){    
         ref.arguments = args;   
     }
     
+    
     int[] array1, array2[]; // array2[] => array2[][]
     int[][] array3;
     int[] array4[], array5[]; // array5[] => array5[][]
     public static void main(String[] args) { 
+        
         byte starting = 3;
         short firstValue = 5; 
         int secondValue = 7; 
@@ -36,7 +39,7 @@ public class ch_1 {
         int i = 1, j = 10;
         do {
            if (i++ > --j) continue;
-        } while (i < 5);
+        }while(i < 5);
         System.out.println("i=" + i + " j=" + j);    
         
         ref = new ch_1(); 
