1104535f66ca99cad9f195fe1bcad64b8bbdd8bd
liho9253 <timmy89566@gmail.com>
2022-02-25 23:06:00 +0800

    20220225

--- Practice_1/src/Tough/ch_2.java
+++ Practice_1/src/Tough/ch_2.java
@@ -15,7 +15,9 @@ public class ch_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        System.out.println();
     }
     
 }
