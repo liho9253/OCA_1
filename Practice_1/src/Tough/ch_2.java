34052be30b3480f2635acb7caa415863dfc0d0cc
liho9253 <timmy89566@gmail.com>
2022-02-28 19:43:22 +0800

    20220228

--- Practice_1/src/Tough/ch_2.java
+++ Practice_1/src/Tough/ch_2.java
@@ -5,19 +5,31 @@
  */
 package Tough;
 
-/**
- *
- * @author user
- */
+// arg 一定會有數
+// 關鍵字不能當標籤 ex: for
+
 public class ch_2 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
+    public int getLoad() {
+        return 1;
+    }
+/*
+    public double getLoad(){ 
+        return 3.0;
+    }*/
     public static void main(String[] args) {
         
-        
+        // int value = 1,000,000;
         System.out.println("...");
+        {
+            System.out.println("no params");
+        }
+        int i = 0;
+      for (i=1 ;  i<5  ; i++) continue;  //(1)
+      for (i=0 ;       ; i++) break;       //(2)
+      for (    ; i<5?false:true ;    );     //(3)
+      
     }
     
 }
