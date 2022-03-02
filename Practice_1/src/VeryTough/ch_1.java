747044e8286ac826c8c599b649f9ecfa753a6135
liho9253 <timmy89566@gmail.com>
2022-03-02 22:19:35 +0800

    20220302

--- Practice_1/src/VeryTough/ch_1.java
+++ Practice_1/src/VeryTough/ch_1.java
@@ -11,11 +11,21 @@ package VeryTough;
  */
 public class ch_1 {
 
-    /**
-     * @param args the command line arguments
-     */
+        static boolean a1;  
+        static boolean b1;   
+        static boolean c1;
     public static void main(String[] args) {
-        // TODO code application logic here
+        Integer i = new Integer(1) ;   
+        Long m = new Long(1);    
+        if( i.equals(m)) System.out.println("equal");     
+        int[] a = { 1 };    
+        a[0] += (a[0] = 4) * (a[0] + 2);
+        System.out.println(a[0]); 
+        // System.out.println(null + true); 
+        // System.out.println(true + null); 
+        // System.out.println(null + null);      
+        boolean bool = (a1 = true) || (b1 = true) && (c1 = true);     
+        System.out.print(a1 + ", " + b1 + ", " + c1);
     }
     
 }
