dc1bbb5fc3716eaca02f5ebbdcae48f78520da89
liho9253 <timmy89566@gmail.com>
2022-03-03 16:20:18 +0800

    20220303

--- Practice_1/src/VeryTough/ch_1.java
+++ Practice_1/src/VeryTough/ch_1.java
@@ -13,7 +13,17 @@ public class ch_1 {
 
         static boolean a1;  
         static boolean b1;   
-        static boolean c1;
+        static boolean c1;   
+        
+        String s1 = "green mile";  
+        
+        public void generateReport( int n ){      
+            String local;
+            if( n > 0 ) local = "good";
+            else local = "bad";
+            System.out.println( s1+" = " + local );  
+        }
+        
     public static void main(String[] args) {
         Integer i = new Integer(1) ;   
         Long m = new Long(1);    
@@ -25,7 +35,19 @@ public class ch_1 {
         // System.out.println(true + null); 
         // System.out.println(null + null);      
         boolean bool = (a1 = true) || (b1 = true) && (c1 = true);     
-        System.out.print(a1 + ", " + b1 + ", " + c1);
+        System.out.println(a1 + ", " + b1 + ", " + c1);  
+        
+        ch_1 s = new GoodSpeak();
+        ((Tone)s).up(); 
+        ((GoodSpeak)s).up(); 
+        
+         // System.out.println("12345".charAt(6));
+}
+ static class GoodSpeak extends ch_1 implements Tone{  
+    public void up(){      
+        System.out.println("UP UP UP");  
     }
-    
+}
+interface Tone{     
+    void up(); 
 }
