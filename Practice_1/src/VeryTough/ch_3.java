4699daafff9b1c0e6272b17794c9f05b28efc2f4
liho9253 <timmy89566@gmail.com>
2022-03-08 11:19:54 +0800

    20220308

--- Practice_1/src/VeryTough/ch_3.java
+++ Practice_1/src/VeryTough/ch_3.java
@@ -1,19 +1,46 @@
 
 package VeryTough;
 
-/**
- *
- * @author user
- */
-public class ch_3 {
+class Baap{   
+    public int h = 4;  
+    public int getH(){ 
+        System.out.println("Baap "+h); return h;    
+    } 
+}
+public class ch_3 extends Baap{
 
-    /**
-     * @param args the command line arguments
-     */
+    // new Long(str) 裡面一定要有數
+    // Boolean.parseBoolean("true") 返回字串  
+    public int h = 44;   
+    public int getH(){ 
+        System.out.println("Beta "+h); return h;  
+    }    
     public static void main(String[] args) {
+        String str = "String";
+        str = str.replace('g','g');
+        System.out.println("String" == str);
         
+        int x  = 0;
+        labelA:   for (int i=10; i<0; i--){
+           int j = 0;
+           labelB:
+           while (j < 10){
+              if (j > i) break labelB;
+              if (i == j){
+                 x++;
+                 continue labelA;
+              }
+              j++;
+           }
+           x--;
+        }
+        System.out.println(x);
         
+        Baap b = new ch_3();
+        System.out.println(b.h+" "+b.getH()); 
         
+        if (false) { x=3; } 
+        // while (false) { x=3; }
     }
     
 }
