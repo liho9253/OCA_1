ddf58c761406491e9ce21cb779b088f44d1d7640
liho9253 <timmy89566@gmail.com>
2022-03-08 11:35:16 +0800

    20220308

--- Practice_1/src/A_809/c_2.java
+++ Practice_1/src/A_809/c_2.java
@@ -5,17 +5,27 @@
  */
 package A_809;
 
-/**
- *
- * @author user
- */
+class MyException extends RuntimeException{}
 public class c_2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    // 抽象方法不能有實作{}
+    // enum 建構子只能用private 且不用 new
     public static void main(String[] args) {
-        // TODO code application logic here
+        if(new Boolean("true")){System.out.println("OK");}
+        if(new Boolean("TRUE")){System.out.println("AOK");}
+        try{
+            m1();
+        }catch(MyException e){
+            System.out.println("A");
+        }
     }
     
+    
+    public static void m1(){
+        try{
+           throw Math.random() > 0.5 ? new MyException():new RuntimeException(); //因為 MyException 繼承 RuntimeException 所以都匯回傳 RuntimeException
+        }catch(RuntimeException e){
+            System.out.println("B");
+        }
+    }
 }
