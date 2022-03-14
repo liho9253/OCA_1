0b95177647fc84c1acafead0d9b8fd66db33dff8
liho9253 <timmy89566@gmail.com>
2022-03-14 23:14:06 +0800

    20220314

--- Practice_1/src/A_809/c_3.java
+++ Practice_1/src/A_809/c_3.java
@@ -11,11 +11,10 @@ package A_809;
  */
 public class c_3 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int i = 0;
+        
     }
     
 }
