b390abcb129066315cc1f3a9d475408e96960932
liho9253 <timmy89566@gmail.com>
2022-03-26 23:23:11 +0800

    20220326

--- Practice_1/src/A_809/c_4.java
+++ Practice_1/src/A_809/c_4.java
@@ -15,7 +15,7 @@ public class c_4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
