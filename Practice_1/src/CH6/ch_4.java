19afd2cbcc7c7e7b55ef13ca27eb9a4304e553ba
liho9253 <timmy89566@gmail.com>
2022-04-05 23:35:46 +0800

    20200405

--- Practice_1/src/CH6/ch_4.java
+++ Practice_1/src/CH6/ch_4.java
@@ -11,9 +11,7 @@ package CH6;
  */
 public class ch_4 {
 
-    /**
-     * @param args the command line arguments
-     */
+    // 1322
     public static void main(String[] args) {
         // TODO code application logic here
     }
