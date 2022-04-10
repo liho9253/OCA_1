a6e2b20235146ca57c6fc47733b522f7acd9c759
liho9253 <timmy89566@gmail.com>
2022-04-10 20:15:24 +0800

    20220410

--- Practice_1/src/A_809/c_3.java
+++ Practice_1/src/A_809/c_3.java
@@ -6,8 +6,20 @@
 package A_809;
 
 public class c_3 {
-
-    
+    // 物件 implements 才用 comparable 比較物件用 comparator
+    // String Buffer 輸出需要 toString
+    // praseInt 可以將字串轉數字
+    // stream .filter 篩選
+    // forEach() 裡面藥用 Comsumer的格式 "-> 或 ::" ex: x ->funD.apply(x) 或是 funD::apply
+    // Function<T,R> 泛型參數要有兩個
+    // ArrayDeque 雙向陣列 
+    // add()往後放  push()往前放  移除都從前面先: remove()會拋出例外 pop()回傳null
+    // Array.asList() 建立之後不能修改元素
+    // allMatch 必須全部true
+    // Predicate<String> 不寫的話預設是 Object
+    // 方法可以有內部類別但是不能有存取權限(public...)
+    // flatMap 資料扁平化
+    // contains 包含
     public static void main(String[] args) {
         int i = 0;
         float q = 2;
