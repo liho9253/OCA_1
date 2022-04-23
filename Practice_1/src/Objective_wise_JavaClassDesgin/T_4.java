11f4f3b8c72f82734122a12f77ac3a48af62d341
liho9253 <timmy89566@gmail.com>
2022-04-23 23:08:56 +0800

    20220423

---
+++ Practice_1/src/Objective_wise_JavaClassDesgin/T_4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objective_wise_JavaClassDesgin;
+
+/**
+ *
+ * @author user
+ */
+public class T_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
