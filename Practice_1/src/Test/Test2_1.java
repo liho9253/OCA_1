dc80a3656ce6f87e0e6e9dc884372f8e6859e5f5
liho9253 <timmy89566@gmail.com>
2022-04-30 01:23:08 +0800

    20220430

--- Practice_1/src/Test/Test2_1.java
+++ Practice_1/src/Test/Test2_1.java
@@ -12,9 +12,12 @@ package Test;
 public class Test2_1 {
 
     // while 宣告的變數只有自己能用 其他也可以宣告同名的變數 互不影響
-    // 
+    // 要讓變數只能讀的話 設為 private 並建立 public getid() 方法 return 該值
+    // override return 的 tpye 要相同
+    
     public static void main(String[] args) {
-        
+        if(false){ }
+        // while(false){}
     }
     
 }
