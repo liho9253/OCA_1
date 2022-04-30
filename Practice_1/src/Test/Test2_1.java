3ae12310164715b6ada1647c6144e39ac5861053
liho9253 <timmy89566@gmail.com>
2022-04-30 22:29:09 +0800

    20220430

--- Practice_1/src/Test/Test2_1.java
+++ Practice_1/src/Test/Test2_1.java
@@ -5,19 +5,30 @@
  */
 package Test;
 
-/**
- *
- * @author user
- */
-public class Test2_1 {
 
+interface I1{
+   int VALUE = 1;
+   void m1();
+}
+interface I2{
+   int VALUE = 2;
+   void m1();
+}
+public class Test2_1  implements I1, I2 {
+    // public class n(){} 只能有一個 public class
     // while 宣告的變數只有自己能用 其他也可以宣告同名的變數 互不影響
     // 要讓變數只能讀的話 設為 private 並建立 public getid() 方法 return 該值
     // override return 的 tpye 要相同
-    
-    public static void main(String[] args) {
+    // interface 可以有 static 但是要有主體
+    // 可以沒有 public
+    // constractors 不能有 return 跟 void
+    // StringBuilder 可修改
+    public void m1() { System.out.println("Hello"); }
+    public static void main(String[] args){
         if(false){ }
         // while(false){}
+      Test2_1 tc = new Test2_1();
+      ( (I1) tc).m1();
     }
     
 }
