4cecc42ac3724ecaffcfd40306321cfd0fde37f2
liho9253 <timmy89566@gmail.com>
2022-05-02 23:35:18 +0800

    20220502

--- Practice_1/src/Test/Test4_1.java
+++ Practice_1/src/Test/Test4_1.java
@@ -11,12 +11,13 @@ package Test;
  */
 public class Test4_1 {
 
-    // =)
-    
-    //=(
-    
+   public static void sayHello(){ System.out.println("Static Hello World"); }  //2
+   // public void sayHello() { System.out.println("Hello World "); }
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
+        // ss
     }
     
 }
