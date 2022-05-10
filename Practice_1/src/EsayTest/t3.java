a0e153a288e034b73f327188e8c1ecce301188d3
liho9253 <timmy89566@gmail.com>
2022-05-10 19:50:41 +0800

    20220510

---
+++ Practice_1/src/EsayTest/t3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EsayTest;
+
+/**
+ *
+ * @author user
+ */
+public class t3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String abc = "";  
+        abc.concat("abc"); 
+        abc.concat("def");  
+        System.out.print(abc);
+        byte b = -128 ;
+        int i = b ;
+    }
+    
+}
